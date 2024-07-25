73c43009c292d8f8ff052991f4f8a1311dce06a6
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-07-25 16:04:43 -0400

    Update retrieve.sh

--- jetson_scripts/retrieve.sh
+++ jetson_scripts/retrieve.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sshpass -p "AFRL1D49." ssh -p 22  afrl@192.168.0.150 'docker stop $(docker ps -a -q); systemctl --user stop docker.container.service; docker stop $(docker ps -a -q)'
+sshpass -p "AFRL1D49." ssh -p 22  afrl@192.168.0.100 'docker stop $(docker ps -a -q); systemctl --user stop docker.container.service; docker stop $(docker ps -a -q)'
+
 mkdir jetson_data
 
 cd jetson_data
@@ -13,7 +16,21 @@ sshpass -p "AFRL1D49." scp -P 22 -r afrl@192.168.0.150:/home/afrl/Desktop/data .
 
 cd .. && mkdir jetson_2
 
+source /opt/ros/humble/setup.bash
+
 cd jetson_2
 
 echo "Pulling data from jetson_2"
 sshpass -p "AFRL1D49." scp -P 22 -r afrl@192.168.0.100:/home/afrl/Desktop/data .
+
+cd ..
+
+source /opt/ros/humble/setup.bash
+
+cd jetson_data
+
+for file in $(find . -type f); do 
+    echo "testing $file"
+    ros2 bag info $file 
+done
+
