6495d9a8383f2b6feeb8b0424da2cad083978ace
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-08-12 16:39:12 -0400

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -2,5 +2,5 @@
 source /opt/ros/humble/setup.bash
 source /ws/install/setup.bash
 cd data
-ros2 launch ros2_bringup stereorig_2.launch.py serial:="'21387972'" sonar:='true'  namespace:='jetson_2' camera_type:="blackfly_s" cam_topic:="debayer/image_raw/rgb"
+ros2 launch ros2_bringup stereorig.launch.py namespace:="jetson_1"
 /bin/bash
